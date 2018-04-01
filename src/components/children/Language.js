825877632c76b7b470e9369ee4e42fec7936fde2
AlejandraCP <alejandracp710@gmail.com>
2018-04-01 16:59:47 -0500

    Add functionality and style mobile

--- src/components/children/Language.js
+++ src/components/children/Language.js
@@ -1,18 +1,20 @@
-import React from 'react'; 
+import React from 'react';
 
-const Language = () =>   (
-  <nav className="ed-item s-30 m-40 l-20 main-end cross-center">
-    <ul className="main-menu ed-menu s-horizontal">
-      <li className="main-menu-item">
-        <a className="main-menu-link" href="#">Español</a>
+const Language = () => (
+  <nav className='ed-item s-30 m-40 l-20 main-end cross-center'>
+    <ul className='main-menu ed-menu s-horizontal'>
+      <li className='main-menu-item'>
+        <a className='main-menu-link' href='#'>
+          Español
+        </a>
       </li>
-      <li className="main-menu-item">
-        <a className="main-menu-link" href="#">Ingles</a>
+      <li className='main-menu-item'>
+        <a className='main-menu-link' href='#'>
+          Ingles
+        </a>
       </li>
     </ul>
   </nav>
-      
-)
- 
+);
 
-export default Language
+export default Language;
