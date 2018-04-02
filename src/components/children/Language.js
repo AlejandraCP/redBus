673fb301250f33f21a123eec59c28945952fe4a2
AlejandraCP <alejandracp710@gmail.com>
2018-04-02 01:07:07 -0500

    languages

--- src/components/children/Language.js
+++ src/components/children/Language.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 const Language = () => (
-  <nav className='ed-item s-30 m-40 l-20 main-end cross-center'>
-    <ul className='main-menu ed-menu s-horizontal'>
-      <li className='main-menu-item'>
-        <a className='main-menu-link' href='#'>
+  <nav className="ed-item s-30 m-40 l-20 main-end cross-center">
+    <ul className="main-menu ed-menu s-horizontal">
+      <li className="main-menu-item">
+        <a
+          className="main-menu-link"
+          href="https://alejandracp.github.io/redBus/"
+        >
           Español
         </a>
       </li>
-      <li className='main-menu-item'>
-        <a className='main-menu-link' href='#'>
+      <li className="main-menu-item">
+        <a
+          className="main-menu-link"
+          href="https://alejandracp.github.io/redBusen/"
+        >
           Ingles
         </a>
       </li>
